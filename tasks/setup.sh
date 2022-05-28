4e57be5ab0b3372b18bc9b1dd33e4e88039e6686
Hubert Ströbitzer <hubstr@gmail.com>
2022-05-28 17:52:23 +0200

    finish

--- tasks/setup.sh
+++ tasks/setup.sh
@@ -26,18 +26,4 @@ sed  '/  volumeMounts:/a \
       mountPath: /apiserver' /root/tmp/apiserver-1.yaml > /root/tmp/apiserver-2.yaml
 mv /root/tmp/apiserver-2.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 
-
-# MANUAL STEPS
-# export IP=$(curl https://ipinfo.io/ip)
-# export API_SERVER=https://$IP:6443
-# PS1="$ "
-# source <(kubectl completion bash)
-# export ETCDCTL_API=3
-# export ETCDCTL_ENDPOINTS=https://127.0.0.1:2379
-# export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
-# export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key
-# export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt
-# kubectl apply -f /root/lines-of-defence/tasks/pod.yaml
-# kubectl create clusterrolebinding my-suboptimal-clusterrolebinding --clusterrole=cluster-admin --serviceaccount default:default
-
 echo "Setup Script Finished Successfuly"
